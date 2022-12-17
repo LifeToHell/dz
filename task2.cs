d59c50161e20804bdb3bf5eeabcc9b88820e1764
LifeToHell <120266818+LifeToHell@users.noreply.github.com>
2022-12-17 20:52:24 +0400

    Add files via upload

---
+++ task2.cs
@@ -0,0 +1,30 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Напишите первое число");
+
+uint a = Convert.ToUInt32(Console.ReadLine());
+
+Console.WriteLine("Напишите второе число");
+
+uint b = Convert.ToUInt32(Console.ReadLine());
+
+Console.WriteLine("Напишите треть число");
+
+uint c = Convert.ToUInt32(Console.ReadLine());
+
+uint max = a;
+
+if(b > a) { max = b;
+
+}
+if(c > b) { max = c;
+
+}
+
+Console.WriteLine(max);
+
+
